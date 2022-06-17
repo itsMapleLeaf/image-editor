e25f88206ce534e213b94a6a67aba9ca7e4e9ea2
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-06-17 17:11:59 -0500

    move editor state into editor component

--- src/editor/editor-state.ts
+++
@@ -1,8 +0,0 @@
-import { FrameState } from "../frame/frame-state"
-import { SpriteState } from "../sprite/sprite-state"
-
-export type EditorState = {
-  frame: FrameState
-  sprites: SpriteState[]
-  selectedSpriteId?: string
-}
