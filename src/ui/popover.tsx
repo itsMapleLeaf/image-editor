8e88c95227b15a3d79daee5968417bfd7d441689
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-06-17 13:03:42 -0500

    improved popover using Dialog for focus trapping

--- src/ui/popover.tsx
+++ src/ui/popover.tsx
@@ -1,11 +1,15 @@
-import { Popover as HeadlessPopover, Transition } from "@headlessui/react"
-import type { ReactNode } from "react"
-import { useState } from "react"
+import { Dialog, Transition } from "@headlessui/react"
+import { Fragment, ReactNode, useState } from "react"
 import { usePopper } from "react-popper"
-import { Slot } from "../react/slot"
-import { Portal } from "./portal"
 
-export function Popover(props: { button: ReactNode; panel: ReactNode }) {
+export function Popover(props: {
+  button: (props: {
+    onClick: () => void
+    ref: (element: Element | null) => void
+  }) => ReactNode
+  panel: ReactNode
+}) {
+  const [isOpen, setIsOpen] = useState(false)
   const [referenceElement, setReferenceElement] = useState<Element | null>()
   const [popperElement, setPopperElement] = useState<HTMLElement | null>()
 
@@ -22,36 +26,44 @@ export function Popover(props: { button: ReactNode; panel: ReactNode }) {
   })
 
   return (
-    <HeadlessPopover>
-      {({ open }) => (
-        <>
-          <HeadlessPopover.Button as={Slot} ref={setReferenceElement}>
-            {props.button}
-          </HeadlessPopover.Button>
-          <Transition.Root show={open}>
-            <Portal>
-              <div
-                ref={setPopperElement}
-                style={styles.popper}
-                {...attributes.popper}
-              >
-                <Transition.Child
-                  enter="transition duration ease-out"
-                  enterFrom="transform scale-95 opacity-0"
-                  enterTo="transform scale-100 opacity-100"
-                  leave="transition duration ease-out"
-                  leaveFrom="transform scale-100 opacity-100"
-                  leaveTo="transform scale-95 opacity-0"
-                >
-                  <HeadlessPopover.Panel static>
-                    {props.panel}
-                  </HeadlessPopover.Panel>
-                </Transition.Child>
-              </div>
-            </Portal>
-          </Transition.Root>
-        </>
-      )}
-    </HeadlessPopover>
+    <>
+      {props.button({
+        onClick: () => setIsOpen(true),
+        ref: setReferenceElement,
+      })}
+      <Transition as={Fragment} show={isOpen}>
+        <Dialog onClose={setIsOpen}>
+          <Transition.Child
+            as={Fragment}
+            enter="transition ease-out"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="transition ease-out"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Dialog.Overlay className="fixed inset-0 bg-black/25" />
+          </Transition.Child>
+
+          <div
+            ref={setPopperElement}
+            style={styles.popper}
+            {...attributes.popper}
+          >
+            <Transition.Child
+              as={Fragment}
+              enter="transition ease-out"
+              enterFrom="transform scale-95 opacity-0"
+              enterTo="transform scale-100 opacity-100"
+              leave="transition ease-out"
+              leaveFrom="transform scale-100 opacity-100"
+              leaveTo="transform scale-95 opacity-0"
+            >
+              <Dialog.Panel>{props.panel}</Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
   )
 }
