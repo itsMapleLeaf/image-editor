592a054fa1d9a51acdabd80babe6757f7b22481e
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-06-18 12:25:09 -0500

    install mobx w/ eslint plugin and overrides

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,10 @@ require("@rushstack/eslint-patch/modern-module-resolution")
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   plugins: ["mobx"],
-  extends: [require.resolve("@itsmapleleaf/configs/eslint")],
+  extends: [
+    require.resolve("@itsmapleleaf/configs/eslint"),
+    "plugin:mobx/recommended",
+  ],
   ignorePatterns: [
     "**/node_modules/**",
     "**/build/**",
@@ -16,4 +19,12 @@ module.exports = {
   parserOptions: {
     project: require.resolve("./tsconfig.json"),
   },
+  overrides: [
+    {
+      files: ["src/{ui,react,dom}/**/*"],
+      rules: {
+        "mobx/missing-observer": "off",
+      },
+    },
+  ],
 }
