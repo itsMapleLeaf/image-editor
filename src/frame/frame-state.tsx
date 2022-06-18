0b04bf2f06f63cdbf3cea1aed691e674356a4c80
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-06-18 14:35:59 -0500

    move state to mobx stores

--- src/frame/frame-state.tsx
+++ src/frame/frame-state.tsx
@@ -1,4 +1,18 @@
-export type FrameState = {
-  width: number
-  height: number
+import { makeAutoObservable } from "mobx"
+
+export class FrameState {
+  width = 640
+  height = 360
+
+  constructor() {
+    makeAutoObservable(this)
+  }
+
+  setWidth = (width: number) => {
+    this.width = width
+  }
+
+  setHeight = (height: number) => {
+    this.height = height
+  }
 }
